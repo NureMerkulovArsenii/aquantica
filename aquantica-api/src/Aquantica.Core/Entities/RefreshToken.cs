36b8144be639a5da8d3bdc136e28378d883c9c32
Arsenii Merkulov <arsenii.vm@gmail.com>
2023-12-11 01:39:57 +0200

    repair appcontext

--- aquantica-api/src/Aquantica.Core/Entities/RefreshToken.cs
+++ aquantica-api/src/Aquantica.Core/Entities/RefreshToken.cs
@@ -5,6 +5,7 @@ namespace Aquantica.Core.Entities;
 [Table("RefreshTokens")]
 public class RefreshToken : BaseEntity
 {
+    public int UserId { get; set; }
     public virtual User User { get; set; }
     public string Token { get; set; }
     public DateTime ExpireDate { get; set; }
